6e0abc303acf5e6d87e7f8c2f7ba0a9ed583a07e
almajulioo <alma.122140057@student.itera.ac.id>
2024-12-06 07:31:31 +0700

    change to https

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
-  {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+  <link rel="stylesheet" href="{{ str_replace('http://', 'https://', Vite::asset('resources/css/app.css')) }}">
+  <script type="module" src="{{ str_replace('http://', 'https://', Vite::asset('resources/js/app.js')) }}"></script>
+
 </head>
 <body class="bg-[#F1F5FF] min-h-screen bg-login bg-no-repeat bg-left-bottom pl-16 pr-[112px]">
   <nav class=" py-8">
